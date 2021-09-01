d81717ea9fe73d0076ab730dc8cea98e6b78cacb
Joshsacabon <sacabonj@gmail.com>
2021-09-01 19:25:02 +0800

    Adding Textbox

--- todolist/App.js
+++ todolist/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, Platform, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
@@ -18,11 +18,22 @@ export default function App() {
             <Task text={'Task 2'} />
             <Task text={'Task 3'} />
             <Task text={'Task 4'} />
-    
             
           </View>
+                   
+          <KeyboardAvoidingView 
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={styles.writeTaskWrapper}
+            >
+             <TextInput style={styles.input} placeholder={"Write a Task"} />
 
+            <TouchableOpacity>
+              <View style={ styles.AddWrapper}> 
+                <Text style={styles.addText}>+</Text>
+              </View>
+            </TouchableOpacity>
 
+            </KeyboardAvoidingView>
         </View>
 
     </View>
@@ -35,8 +46,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#E8EAED',
   },
   tasksWrapper: {
-    paddingTop:80,
-    paddingHorizontal:20,
+    paddingTop: 80,
+    paddingHorizontal: 20,
   },
   sectionTitle: {
     fontSize: 24,
@@ -45,5 +56,37 @@ const styles = StyleSheet.create({
   },
   items:{
     marginTop: 30, 
-  }
+  },
+  writeTaskWrapper:{
+    position: 'absolute',
+    bottom: -320,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  input:{
+    paddingVertical: 15,
+    width: 250,
+    paddingHorizontal: 15,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    borderColor: '#c0c0c0',
+    borderWidth: 1,
+
+  },
+  AddWrapper:{
+    width: 60,
+    height: 60,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#c0c0c0',
+    borderWidth: 1,
+  },
+  addText:{
+
+  },
+
 });
